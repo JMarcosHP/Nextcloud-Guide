940c9bdeced034a87eb962d47cf9ab96f1b47e4f
JMarcosHP <jehuherrerap@hotmail.com>
2025-08-09 10:29:42 -0600

    Add update-apps cron script
    Signed-off-by: JMarcosHP <jehuherrerap@hotmail.com>

---
+++ nextconf/cron/update-apps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to update all Nextcloud apps automatically. Needs to run as www-data user.
+# sudo -E -u www-data
+# Or set an automated cronjob:
+# sudo crontab -u www-data -e
+#
+# Please don't execute this script during maintenance mode operations, as is discouraged by the Nextcloud manual.
+
+php -f /var/www/nextcloud/occ app:update --all
+
